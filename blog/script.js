2083ac8eec1fd5560722eafde8191094b315b0ec
saijaku1 <souta20101024@outlook.jp>
2024-11-01 20:12:17 +0900

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,130 +1,47 @@
-   const articlesData = [
+  const posts = [
     {
         title: "うししし。今日のブログはこのサイトの大型アップデート！",
-        content: "(*￣▽￣)ﾌﾌﾌｯ♪。どうもこんにちは。超上機嫌な<b>じゃく</b>です。なんでかって？その理由は...",
-        date: "10月28日に更新済み",
+        content: "( *￣▽￣)ﾌﾌﾌｯ♪。どうもこんにちは。超上機嫌な<b>じゃく</b>です。なんでかって？その理由は...",
+        date: "10月28日",
         link: "https://mysite.f5.si/blog/update"
     },
     {
         title: "最弱のアニメのスタイルを変えました。",
         content: "最弱のアニメのスタイルを変更しました。見えにくいというフォームのリクエストがあったので、変更しました。",
-        date: "10月26日に更新済み",
-        link: ""
+        date: "10月26日"
     },
     {
         title: "すべてのリポジトリのURLを変更しました。",
-        content: "Githubのリポジトリ(blog-mysite.f5.siなど)のURLをすべてmysite.f5.siに移行したせいで、アクセスができなくなりました。10月16日のようにご迷惑をかけて申し訳ありません。<br>前 blog-mysite.f5.si←廃止！<br>今 mysite.f5.si/blog←移行！<br>URLの最後にblogやmadeなどが追加されただけです。サイトのスタイルなどは変わっていないのでご安心してください。<br>",
-        date: "10月22日に更新済み",
-        link: ""
+        content: "Githubのリポジトリ(blog-mysite.f5.siなど)のURLをすべてmysite.f5.siに移行したせいで、アクセスができなくなりました。10月16日のようにご迷惑をかけて申し訳ありません。<br><b>前</b> blog-mysite.f5.si←廃止！<br><b>今</b> mysite.f5.si/blog←移行！<br>URLの最後にblogやmadeなどが追加されただけです。サイトのスタイルなどは変わっていないのでご安心してください。",
+        date: "10月22日"
     },
     {
         title: "サイトが喋ってくれるサイトを作りました！",
         content: "サイトが喋ってくれる(不自然)なサイトを作ってみました！ぜひ見てください！",
-        date: "10月20日に更新済み",
+        date: "10月20日",
         link: "https://mysite.f5.si/made/speak"
     },
     {
         title: "リンクの色をずっと青色になるように変更しました",
         content: "リンクが一部紫色になっていたので、クリックしてもずっと青色のまま表示されるようにスタイルを変更しました。",
-        date: "10月16日に更新済み",
-        link: ""
+        date: "10月16日"
     },
     {
         title: "URLを変更しました。",
-        content: "Githubのフォルダの追加により、アクセスができなくなったため、URLを一部変更しました。<br>前 名前.f5.si<br>今 名前.f5.si/html<br>URLの最後にhtmlが追加されただけです。サイトのスタイルなどは変わっていないのでご安心してください。<br>迷惑をかけて申し訳ありません。",
-        date: "10月16日に更新済み",
-        link: ""
+        content: "Githubのフォルダの追加により、アクセスができなくなったため、URLを一部変更しました。<br><b>前</b> 名前.f5.si<br><b>今</b> 名前.f5.si/html<br>URLの最後にhtmlが追加されただけです。サイトのスタイルなどは変わっていないのでご安心してください。<br>迷惑をかけて申し訳ありません。",
+        date: "10月16日"
     },
     {
         title: "このサイトの秘密 Part2ができました。",
         content: "Part2を投稿しました！Part1とは違いますが、ぜひ見てください！Part2は<b>ドメインの取得</b>の仕方です。",
-        date: "10月16日に更新済み",
+        date: "10月16日",
         link: "https://mysite.f5.si/blog/himitsu2"
     },
     {
         title: "電卓を作りました。",
-        content: "電卓ができました。ぜひ使ってみてください！※掛け・割り・足し・引き算しかできません。cosなどの三角関数は現在開発中です...",
-        date: "10月15日に更新済み",
+        content: "電卓ができました。ぜひ使ってみてください！※一部使えない関数があります。",
+        date: "10月15日",
         link: "https://mysite.f5.si/made/calculator"
-    },
-    {
-        title: "アニメサイトを作りました！",
-        content: "僕のアニメのサイトができました！まだ第一話しか公開していないですが、是非見てください！",
-        date: "10月15日に更新済み",
-        link: "https://mysite.f5.si/anime"
-    },
-    {
-        title: "じゃくの悩み事",
-        content: "じゃくがあることに悩んでいます。つまらないかもしれないですが、どうか聞いてあげてください。",
-        date: "10月14日に更新済み",
-        link: "https://mysite.f5.si/blog/悩み事"
-    },
-    {
-        title: "じゃくのプロフィールができました!",
-        content: "ついに僕のことが暴かれるのか...恥ずかしいーーー",
-        date: "10月14日に更新済み",
-        link: "https://mysite.f5.si/blog/about"
-    },
-    {
-        title: "最弱のオリジナルキャラクターができました!",
-        content: "こんにちは。今日からこんな感じにチャットしていこうと思います。あとかわいい......",
-        date: "10月13日に更新済み",
-        link: "https://mysite.f5.si/blog/キャラクター"
-    },
-    {
-        title: "最弱がまた変なサイトを作りました。",
-        content: "僕、最弱がまた変なサイトを作りました。\"おみくじ\"です。今日のあなたの運はどうなんでしょうか？それでは試していきましょう！",
-        date: "10月10日に更新済み",
-        link: "https://mysite.f5.si/made/おみくじ"
-    },
-    {
-        title: "このサイトのプログラムの謎！ Part1",
-        content: "みなさん、ウェブサイトを開くとき、その裏側でどんな仕組みが動いているのか考えたことはありますか？「サイトのプログラミングってどんな仕組みなんだろう？」と疑問に思ったことがある方も多いかもしれません。実は、私たちが普段見ているウェブページは...",
-        date: "10月8日に更新済み",
-        link: "https://mysite.f5.si/blog/himitsu"
-    },
-    {
-        title: "最弱が作ったサイトを公開しました！",
-        content: "「最弱が作ったサイト」では、僕が制作した様々なサイトを掲載しています。変わったサイトから、役立つ情報を提供するサイトまで、多岐にわたる内容をお楽しみいただけます。また、サイトの利用方法や新しいコンテンツについての情報も随時更新していきますので、ぜひお気軽に訪れてみてください。",
-        date: "10月7日に更新済み",
-        link: ""
-    },
-    {
-        title: "最弱のサイトのスタイルなどを変更しました。",
-        content: "あまりにも記事が見えにくくなったので、全体的なサイトのスタイルを変更しました。これで見やすくなったと思います！",
-        date: "10月7日に更新済み",
-        link: ""
-    },
-    {
-        title: "僕がやってるRide a cart Simulatorについて",
-        content: "Q1. アップグレードとは？A1. アップグレードとは等級・品質などを上げること。アップグレードにはネオン、ゴールド、レインボー、ダークマターの4つに分けられている。...",
-        date: "10月3日に更新済み",
-        link: "https://mysite.f5.si/blog/ride"
-    },
-    {
-        title: "サイトが完成！",
-        content: "10月2日にメインサイトが完成しました。制作期間は1ヶ月ほどかかりました！これからまだまだ更新をしていきますので、このサイトをよろしくお願い致します。",
-        date: "10月2日に更新済み",
-        link: ""
     }
 ];
- const articlesContainer = document.getElementById("articles");
-    articlesContainer.innerHTML = articlesData.map(article => `
-        <div class="article">
-            <h2>${article.title}</h2>
-            <p>${article.content}</p>
-            <h3>${article.date}に更新済み</h3>
-            <a href="https://mysite.f5.si/blog/${article.link}">続きを読む</a>
-        </div>
-    `).join("");
-    setTimeout(() => {
-        document.getElementById("loading").style.display = "none";
-        articlesContainer.style.opacity = "1";
-        articlesContainer.style.transform = "translateY(0)";
-    }, 1000);
 
-    const sidebar = document.getElementById("sidebar");
-    document.getElementById("toggleButton").addEventListener("click", () => {
-        sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
-    });
-});
