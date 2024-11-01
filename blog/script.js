edffa7c0b0e585da313c09b85d608f9ea643948a
saijaku1 <souta20101024@outlook.jp>
2024-11-01 20:28:48 +0900

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,47 +1,29 @@
-  const posts = [
-    {
-        title: "うししし。今日のブログはこのサイトの大型アップデート！",
-        content: "( *￣▽￣)ﾌﾌﾌｯ♪。どうもこんにちは。超上機嫌な<b>じゃく</b>です。なんでかって？その理由は...",
-        date: "10月28日",
-        link: "https://mysite.f5.si/blog/update"
-    },
-    {
-        title: "最弱のアニメのスタイルを変えました。",
-        content: "最弱のアニメのスタイルを変更しました。見えにくいというフォームのリクエストがあったので、変更しました。",
-        date: "10月26日"
-    },
-    {
-        title: "すべてのリポジトリのURLを変更しました。",
-        content: "Githubのリポジトリ(blog-mysite.f5.siなど)のURLをすべてmysite.f5.siに移行したせいで、アクセスができなくなりました。10月16日のようにご迷惑をかけて申し訳ありません。<br><b>前</b> blog-mysite.f5.si←廃止！<br><b>今</b> mysite.f5.si/blog←移行！<br>URLの最後にblogやmadeなどが追加されただけです。サイトのスタイルなどは変わっていないのでご安心してください。",
-        date: "10月22日"
-    },
-    {
-        title: "サイトが喋ってくれるサイトを作りました！",
-        content: "サイトが喋ってくれる(不自然)なサイトを作ってみました！ぜひ見てください！",
-        date: "10月20日",
-        link: "https://mysite.f5.si/made/speak"
-    },
-    {
-        title: "リンクの色をずっと青色になるように変更しました",
-        content: "リンクが一部紫色になっていたので、クリックしてもずっと青色のまま表示されるようにスタイルを変更しました。",
-        date: "10月16日"
-    },
-    {
-        title: "URLを変更しました。",
-        content: "Githubのフォルダの追加により、アクセスができなくなったため、URLを一部変更しました。<br><b>前</b> 名前.f5.si<br><b>今</b> 名前.f5.si/html<br>URLの最後にhtmlが追加されただけです。サイトのスタイルなどは変わっていないのでご安心してください。<br>迷惑をかけて申し訳ありません。",
-        date: "10月16日"
-    },
-    {
-        title: "このサイトの秘密 Part2ができました。",
-        content: "Part2を投稿しました！Part1とは違いますが、ぜひ見てください！Part2は<b>ドメインの取得</b>の仕方です。",
-        date: "10月16日",
-        link: "https://mysite.f5.si/blog/himitsu2"
-    },
-    {
-        title: "電卓を作りました。",
-        content: "電卓ができました。ぜひ使ってみてください！※一部使えない関数があります。",
-        date: "10月15日",
-        link: "https://mysite.f5.si/made/calculator"
-    }
-];
+   window.addEventListener("load", function () {
+  setTimeout(load, 1000); 
+  function load() {
+    document.body.classList.add("loaded");
+  }
+});
+function toggleSidebar() {
+      var sidebar = document.getElementById("sidebar");
+      var toggleButton = document.getElementById("toggleButton");
 
+      if (sidebar.classList.contains("open")) {
+        sidebar.classList.remove("open");  // サイドバーを閉じる
+        toggleButton.innerHTML = "☰";     // ボタンを「☰」に戻す
+      } else {
+        sidebar.classList.add("open");     // サイドバーを開く
+        toggleButton.innerHTML = "×";      // ボタンを「×」に変更
+      }
+    }
+window.onscroll = function() {
+    const button = document.getElementById("topButton");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+document.getElementById("topButton").onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
