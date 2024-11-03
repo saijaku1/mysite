1b8958a498000c065c5b5c93b320797188995318
saijaku1 <souta20101024@outlook.jp>
2024-11-04 01:18:45 +0900

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+const button=document.querySelector("button").addEventListener("click",()=>{
+  window.location.href="https://mysite.f5.si";
+})
+const next=document.querySeector("#next").addEventListener("click",=>(){
+window.location.href="/terms-of-use";
+})
