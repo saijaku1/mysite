61d71dcb75e18672c81d95dae90e0617b82ced56
saijaku1 <souta20101024@outlook.jp>
2024-11-04 08:46:57 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,5 +2,5 @@ const button=document.querySelector("button").addEventListener("click",()=>{
   window.location.href="https://mysite.f5.si";
 })
 const next=document.querySeector("#next").addEventListener("click",=>(){
-window.location.href="/terms-of-use";
+window.location.href="/history";
 })
