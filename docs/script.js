7e734308f73a0e77a8e81f34b603d6c316132a51
saijaku1 <souta20101024@outlook.jp>
2024-11-04 14:20:43 +0900

    Delete docs/script.js

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-const button=document.querySelector("button").addEventListener("click",()=>{
-  window.location.href="https://mysite.f5.si";
-})
-const next=document.querySelector("#next").addEventListener("click",=>(){
-window.location.href="/history";
-})
