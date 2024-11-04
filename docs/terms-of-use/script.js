e7aad36cb95934e13ab78875328ff44fe6405e23
saijaku1 <souta20101024@outlook.jp>
2024-11-04 23:47:29 +0900

    Delete docs/terms-of-use/script.js

--- docs/terms-of-use/script.js
+++
@@ -1,7 +0,0 @@
-const button=document.querySelector("button").addEventListener("click",()=>{
-  window.location.href="https://mysite.f5.si";
-})
-
-const before=document.querySelector("#before").addEventListener("click",()=>{
-window.location.href="https://mysite.f5.si/docs/history";
-})
