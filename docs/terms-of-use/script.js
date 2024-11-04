d127518f4c6ec1a1e0ab0223d840d48a0d97116e
saijaku1 <souta20101024@outlook.jp>
2024-11-04 23:42:22 +0900

    Create script.js

---
+++ docs/terms-of-use/script.js
@@ -0,0 +1,7 @@
+const button=document.querySelector("button").addEventListener("click",()=>{
+  window.location.href="https://mysite.f5.si";
+})
+
+const before=document.querySelector("#before").addEventListener("click",=>(){
+window.location.href="https://mysite.f5.si/docs/history";
+})
